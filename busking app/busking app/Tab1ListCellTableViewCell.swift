3db1526483c89ea6786c2d8c6f36e2695dd02ebf
이서영 <seoyeong9999@gmail.com>
2018-08-06 14:09:22 +0900

    ddddd

---
+++ busking app/busking app/Tab1ListCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Tab1ListCellTableViewCell.swift
+//  busking app
+//
+//  Created by 방문사용자 on 2018. 8. 6..
+//  Copyright © 2018년 kr.ac.korea. All rights reserved.
+//
+
+import UIKit
+
+class Tab1ListCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
