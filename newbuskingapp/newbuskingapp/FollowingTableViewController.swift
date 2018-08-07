2fa4f280bfcd95d79b0428311a619db9c8c2bcb1
조준형 <jun5905>
2018-08-07 16:40:37 +0900

    busking

--- newbuskingapp/newbuskingapp/FollowingTableViewController.swift
+++ newbuskingapp/newbuskingapp/FollowingTableViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-struct Favorite {
+struct Following {
     var busker: String
     var hashtag: String
     var followers: String
@@ -8,14 +8,14 @@ struct Favorite {
 }
 
 
-class FavoriteTableViewController: UITableViewController {
+class FollowingTableViewController: UITableViewController {
     
     var data = [
-        Favorite(busker: "김고양", hashtag: "#Piano #Ballad #solo", followers: "8231", profileImage: "cat"),
-        Favorite(busker: "김안경", hashtag: "#Folk #Ballad #Dance #solo", followers: "5096", profileImage: "10cm"),
-        Favorite(busker: "김로이", hashtag: "#Ballad #Drama #OST", followers: "4342", profileImage: "roy"),
-        Favorite(busker: "선우정", hashtag: "#싱어송라이터 #독특한 가사", followers: "4234", profileImage: "sun"),
-        Favorite(busker: "노서우", hashtag: "#싱어송라이터 #기타", followers: "3244", profileImage: "profile")
+        Following(busker: "김고양", hashtag: "#Piano #Ballad #solo", followers: "43", profileImage: "cat"),
+        Following(busker: "김안경", hashtag: "#Folk #Ballad #Dance #solo", followers: "121", profileImage: "10cm"),
+        Following(busker: "김로이", hashtag: "#Ballad #Drama #OST", followers: "235", profileImage: "roy"),
+        Following(busker: "선우정", hashtag: "#싱어송라이터 #독특한 가사", followers: "45", profileImage: "sun"),
+        Following(busker: "노서우", hashtag: "#싱어송라이터 #기타", followers: "143", profileImage: "profile")
         
     ]
     
@@ -32,7 +32,7 @@ class FavoriteTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteTableViewCell", for: indexPath) as! FavoriteTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FollowingTableViewCell", for: indexPath) as! FollowingTableViewCell
         
         let item = data[indexPath.row]
         
@@ -52,6 +52,7 @@ class FavoriteTableViewController: UITableViewController {
         
         return data.count
     }
-
+    
 }
+
 
