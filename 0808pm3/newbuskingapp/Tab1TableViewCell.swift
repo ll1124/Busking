4c797bbe47592102c079cf55b305d888a5bc8021
seoyeong99 <seoyeong9999@gmail.com>
2018-08-08 15:38:15 +0900

    0808pm3

---
+++ 0808pm3/newbuskingapp/Tab1TableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  Tab1TableViewCell.swift
+//  newbuskingapp
+//
+//  Created by 방문사용자 on 06/08/2018.
+//  Copyright © 2018 방문사용자. All rights reserved.
+//
+
+import UIKit
+
+class Tab1TableViewCell: UITableViewCell {
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var place: UILabel!
+    @IBOutlet weak var artist: UILabel!
+    @IBOutlet weak var heartButton: UIButton!
+    @IBOutlet weak var heartCount: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
