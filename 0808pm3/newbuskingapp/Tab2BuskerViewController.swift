3742c25eeb4b53f5736c7ff32111c46946a91d5d
seoyeong99 <seoyeong9999@gmail.com>
2018-08-08 18:18:55 +0900

    0808 마무리

---
+++ 0808pm3/newbuskingapp/Tab2BuskerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  Tab2BuskerViewController.swift
+//  newbuskingapp
+//
+//  Created by 방문사용자 on 2018. 8. 8..
+//  Copyright © 2018년 방문사용자. All rights reserved.
+//
+
+import UIKit
+
+class Tab2BuskerViewController: UIViewController {
+
+    @IBOutlet weak var tab2BuskerTable: UITableView!
+    @IBOutlet weak var tab2SearchBar: UISearchBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
